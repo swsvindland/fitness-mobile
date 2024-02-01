298c950337d588a0a960a869f65c97fd75568f3d
Sam Svindland <swsvindland@gmail.com>
2024-01-31 23:23:32 -0500

    update body dependencies

--- apps/body_track/lib/main.dart
+++ apps/body_track/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:body_track/utils/colors.dart';
-import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -17,9 +16,6 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
-  );
-  await FirebaseAppCheck.instance.activate(
-    webRecaptchaSiteKey: 'recaptcha-v3-site-key',
   );
   runApp(const App());
 }
