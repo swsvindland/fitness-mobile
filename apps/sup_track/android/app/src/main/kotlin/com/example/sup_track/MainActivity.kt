b47876c786c7224c5a38699aa1a6bde0f89eec88
Sam Svindland <swsvindland@gmail.com>
2024-02-02 14:48:39 -0500

    remove sup track for now

--- apps/sup_track/android/app/src/main/kotlin/com/example/sup_track/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.sup_track
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- apps/sup_track/lib/utils/colors.dart
+++
@@ -1,3 +0,0 @@
-import 'dart:ui';
-
-const primary = Color(0xff14B8A6);
