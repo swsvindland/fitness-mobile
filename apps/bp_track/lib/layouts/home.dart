9ff4da2f5e224d39ff1a0cd1d955deaf77dac7fe
Sam Svindland <swsvindland@gmail.com>
2024-02-13 20:45:42 -0500

    upgrade period track gradle

--- apps/bp_track/lib/layouts/home.dart
+++ apps/bp_track/lib/layouts/home.dart
@@ -1,11 +1,12 @@
 import 'package:api/body_database_service.dart';
 import 'package:bp_track/widgets/app_bar_ad.dart';
+import 'package:bp_track/widgets/navigation/navigation_bottom.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 import '../widgets/all.dart';
 import '../widgets/check_in_form.dart';
 import '../widgets/home.dart';
+import '../widgets/navigation/side_navigation.dart';
 import '../widgets/settings.dart';
 
 class HomePage extends StatefulWidget {
@@ -40,39 +41,44 @@ class _HomeState extends State<HomePage> {
         title: const AppBarAd(),
         elevation: 0,
       ),
-      body: Container(
-        padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 16),
-        child: currentPageIndex == 0
-            ? Home()
-            : currentPageIndex == 1
-                ? All()
-                : Settings(),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          SideNavigation(
+            selectedIndex: currentPageIndex,
+            onItemTapped: (index) {
+              setState(() {
+                currentPageIndex = index;
+              });
+            },
+          ),
+          Expanded(
+            child: Align(
+              alignment: Alignment.topCenter,
+              child: SizedBox(
+                width: 600,
+                child: currentPageIndex == 0
+                    ? Home()
+                    : currentPageIndex == 1
+                    ? All()
+                    : Settings(),
+              ),
+            ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: handleAction,
         child: const Icon(Icons.add),
       ),
-      bottomNavigationBar: NavigationBar(
-        onDestinationSelected: (int index) {
+      bottomNavigationBar: NavigationBottom(
+        selectedIndex: currentPageIndex,
+        onItemTapped: (index) {
           setState(() {
             currentPageIndex = index;
           });
         },
-        selectedIndex: currentPageIndex,
-        destinations: <Widget>[
-          NavigationDestination(
-            icon: const Icon(Icons.home),
-            label: AppLocalizations.of(context)!.home,
-          ),
-          NavigationDestination(
-            icon: const Icon(Icons.insights),
-            label: AppLocalizations.of(context)!.all,
-          ),
-          NavigationDestination(
-            icon: const Icon(Icons.settings),
-            label: AppLocalizations.of(context)!.settings,
-          ),
-        ],
       ),
     );
   }
