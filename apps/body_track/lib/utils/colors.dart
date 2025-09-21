e97260cf6df8c8141af55e342cf131607b330a34
Sam Svindland <swsvindland@gmail.com>
2025-09-21 12:45:40 -0400

    feat: Update color scheme and add encryption setting
    This commit introduces a new color scheme for the Body Track app and adds a setting to Info.plist to indicate that the app uses non-exempt encryption.
    
    The new color scheme includes:
    - `background`: 0xff0D3140
    - `card`: 0xff2E586A
    - `primary`: 0xff28827A
    - `secondary`: 0xffF7C619
    - `ternary`: 0xffAFD257
    - `error`: 0xffba1a1a
    
    The `ITSAppUsesNonExemptEncryption` key is set to `false` in Info.plist.

--- apps/body_track/lib/utils/colors.dart
+++ apps/body_track/lib/utils/colors.dart
@@ -1,4 +1,9 @@
 import 'dart:ui';
 
-const primary = Color(0xffD946EF);
-const secondary = Color(0xff86198f);
+const background = Color(0xff0D3140);
+const card = Color(0xff2E586A);
+
+const primary = Color(0xff28827A);
+const secondary = Color(0xffF7C619);
+const ternary = Color(0xffAFD257);
+const error = Color(0xffba1a1a);
